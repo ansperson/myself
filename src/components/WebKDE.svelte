ab1f770218ba8386fea3978e4ec4b9eb06ee039c
Alan Pereira <ans.person@gmail.com>
2025-12-30 17:15:46 +0000

    fix: pipeline and gui load

--- src/components/WebKDE.svelte
+++ src/components/WebKDE.svelte
@@ -1,7 +1,11 @@
+<script>
+  const baseUrl = import.meta.env.BASE_URL;
+</script>
+
 <div class="h-full w-full bg-black">
   <iframe
     title="webKDE desktop"
-    src="/webkde/index.html"
+    src={`${baseUrl}webkde/index.html`}
     class="h-full w-full border-0"
   ></iframe>
 </div>
